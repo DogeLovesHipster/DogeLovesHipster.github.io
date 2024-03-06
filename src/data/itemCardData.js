01c5bbae7ed513b3d8807f887affa5126fa7c30a
Sergio <shadow41402@gmail.com>
2024-03-05 22:10:50 -0800

    Added more sounds

--- src/data/itemCardData.js
+++ src/data/itemCardData.js
@@ -1,9 +1,11 @@
 import Cockaded1 from '../assets/images/cockaded_1.jpg';
 import Cockaded2 from '../assets/images/cockaded_2.jpg';
 import Cockaded3 from '../assets/images/cockaded_3.jpg';
+import Cockaded4 from '../assets/images/cockaded_4.jpg';
 import call1 from "../assets/sounds/Calls1.mp3";
 import call2 from "../assets/sounds/Calls2.mp3";
 import call3 from "../assets/sounds/Calls3.mp3";
+import call4 from "../assets/sounds/Calls4.mp3";
 import drum1 from "../assets/sounds/Drum1.mp3";
 import juvenileCall1 from "../assets/sounds/JuvenileCall1.mp3";
 
@@ -13,16 +15,18 @@ const itemCardData = [
     images: [
       Cockaded1,
       Cockaded2,
-      Cockaded3
+      Cockaded3,
+      Cockaded4
     ],
     caption: 'A cute Red-Cockaded Woodpecker chillin.',
     conservationStatus: 'Near Threatened',
     sounds: [
       { src: call1, callType: 'Call 1', date: 'March 02, 2007', citation: 'Recorded by Martha Fischer / Macaulay Library', duration: '0:21' },
-      { src: call2, callType: 'Call 2', date: '2022-01-02', citation: 'Recorded by Jane Doe', duration: '0:45' },
-      { src: call3, callType: 'Call 3', date: '2022-01-03', citation: 'Recorded by John Doe', duration: '1:00' },
-      { src: drum1, callType: 'Drum 1', date: '2022-01-04', citation: 'Recorded by Jane Doe', duration: '0:30' },
-      { src: juvenileCall1, callType: 'Juvenile Call 1', date: '2022-01-05', citation: 'Recorded by John Doe', duration: '0:45' },
+      { src: call2, callType: 'Call 2', date: 'May 21, 2012', citation: 'Recorded by Andrew Spencer', duration: '0:20' },
+      { src: call3, callType: 'Call 3', date: 'May 21, 2012', citation: 'Recorded by Andrew Spencer', duration: '0:23' },
+      { src: call4, callType: 'Call 4', date: 'May 02, 1963', citation: 'Recorded by Robert C. Stein / Macaulay Library', duration: '0:10' },
+      { src: drum1, callType: 'Drum 1', date: 'April 25, 1982', citation: 'Recorded by Rose Ann Rowlett / Macaulay Library', duration: '0:21' },
+      { src: juvenileCall1, callType: 'Juvenile Call 1', date: 'June 04, 2017', citation: 'Recorded by Paul Marvin / Macaulay Library', duration: '0:12' },
     ],
   },
 ];
