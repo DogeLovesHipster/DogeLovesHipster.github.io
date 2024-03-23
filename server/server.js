5c232d7bb6846898bfaca584d2760c73454626b3
Sergio <shadow41402@gmail.com>
2024-03-23 16:58:53 -0700

    Back end addons

--- server/server.js
+++ server/server.js
@@ -1,13 +1,41 @@
 import express from 'express';
+// import cors from 'cors';
+import mongoose from 'mongoose';
 import commentsRouter from '../src/routes/comment.js';
 
-// server.js or app.js
+// Constants
+const PORT = process.env.PORT || 5174;
+const ENV = process.env.NODE_ENV || 'production';
+const DB_CONNECT = process.env.DB_CONNECT || 'mongodb://localhost:27017/wikipediaClone'; 
 
+// Initialize Express
 const app = express();
 
+// Middleware
+// app.use(cors());
 app.use(express.json());
+
+// Initialize DB
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    socketTimeoutMS: 360000,
+  })
+  .then(() => {
+    console.info(`MongoDB connected`);
+  })
+  .catch((err) => {
+    console.error('Mongo Connection Error', err);
+  });
+
+// Routes
 app.use('/api/comments', commentsRouter);
 
-app.listen(5173, () => {
-    console.log('Server is running on port 5173');
+// Start server
+const server = app.listen(PORT, () => {
+  console.info(`Server running on port ${PORT} in ${ENV} environment`);
 });
+
+export { app as default, server };
